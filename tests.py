a697efc0cdfe920f3cf982a541b7784e075d9ab5
Michal <michal.tyszkiewicz@gmail.com>
2019-11-08 13:43:24 +0100

    Move unet to own file, keep __init__.py minimal; rename default_setup to fat_setup

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import torch, unittest
-from unets import default_setup, Unet, ThinUnetUpBlock, ThinUnetDownBlock
+from unets import thin_setup, Unet, ThinUnetUpBlock, ThinUnetDownBlock
 
 class BaseTests(unittest.TestCase):
     def test_inequal_output_asymmetric(self):
@@ -21,32 +21,22 @@ class BaseTests(unittest.TestCase):
 
 class ThinTests(unittest.TestCase):
     def test_inequal_output_asymmetric(self):
-        setup = {
-            **default_setup,
-            'up_block': ThinUnetUpBlock,
-            'down_block': ThinUnetDownBlock,
-        }
         unet = Unet(
             in_features=3,
             out_features=4,
             down=[16, 32, 64],
             up=[40, 24],
-            setup=setup
+            setup=thin_setup
         )
         input = torch.zeros(2, 3, 104, 104)
         output = unet(input)
         self.assertEqual(torch.Size([2, 4, 64, 64]), output.size())
 
     def test_inequal_output_symmetric(self):
-        setup = {
-            **default_setup,
-            'up_block': ThinUnetUpBlock,
-            'down_block': ThinUnetDownBlock,
-        }
         unet = Unet(
             down=[16, 32, 64],
             up=[40, 24],
-            setup=setup
+            setup=thin_setup
         )
         input = torch.zeros(2, 1, 104, 104)
         output = unet(input)
